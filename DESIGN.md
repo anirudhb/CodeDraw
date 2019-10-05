1e8671a7ba83aae32cc2fd9fbb5c0aad55e1d292
anirudhb <anirudhb@users.noreply.github.com>
2019-10-05 13:46:04 -0700

    initial commit

---
+++ DESIGN.md
@@ -0,0 +1,33 @@
+**Theme: STEM**
+
+Idea!!!
+
+App / program to learn coding
+
+Principles:
+* NO KEYBOARD
+* Mouse / touch only
+* Never make them comprehend technicalities
+
+Real-life anecdote:
+* My sister. She hates using the keyboard. She says, why do I need to type all of these things to make something?
+
+Example demo:
+
+Write "ask" on screen
+
+As you say "so first ask what is their name?"
+
+Write "say" on screen
+
+As you say "then say hello and their name"
+
+Then while saying "name" write "!"
+
+Run
+
+Asks: "What is your name?"
+
+User gives input
+
+Says/writes: "Hello \<name>!"
---
+++ codedraw-web/index.js
@@ -0,0 +1,68 @@
+const editorElement = document.getElementById('editor');
+const languageElement = document.getElementById('language');
+const undoElement = document.getElementById('undo');
+const redoElement = document.getElementById('redo');
+const convertElement = document.getElementById('convert');
+editorElement.addEventListener('changed', function(evt) {
+    undoElement.disabled = !evt.detail.canUndo;
+    redoElement.disabled = !evt.detail.canRedo;
+    convertElement.disabled = !evt.detail.canConvert;
+});
+editorElement.addEventListener('loaded', function(evt) {
+    /**
+     * Retrieve the list of available recognition languages
+     * @param {Object} The editor recognition parameters
+     */
+    const currentLanguage = evt.target.editor.configuration.recognitionParams.v4.lang;
+    const res = MyScript.getAvailableLanguageList();
+    Object.keys(res.result).forEach(function(key) {
+        const selected = currentLanguage === key;
+        languageElement.options[languageElement.options.length] = new Option(res.result[key], key, selected, selected);
+    });
+    languageElement.disabled = false;
+});
+languageElement.addEventListener('change', function(e) {
+    const configuration = editorElement.editor.configuration;
+    //The path to the language depend of the version of API you are using.
+    //configuration.recognitionParams.v4.lang = e.target.value;
+    //editorElement.editor.configuration = configuration;
+});
+undoElement.addEventListener('click', function() {
+    editorElement.editor.undo();
+});
+redoElement.addEventListener('click', function() {
+    editorElement.editor.redo();
+});
+convertElement.addEventListener('click', function() {
+        editorElement.editor.convert();
+    })
+    /**
+     * Attach an editor to the document
+     * @param {Element} The DOM element to attach the ink paper
+     * @param {Object} The recognition parameters
+     */
+MyScript.register(editorElement, {
+    recognitionParams: {
+        type: 'DIAGRAM',
+        protocol: 'WEBSOCKET',
+        apiVersion: 'V4',
+        server: {
+            //applicationKey: '006df10c-c66b-4c53-8596-03636a5530d1',
+            //hmacKey: '40645187-9dcc-4997-a55b-13bdeb302158'
+            scheme: 'https',
+            host: 'webdemoapi.myscript.com',
+            applicationKey: secrets.applicationKey,
+            hmacKey: secrets.hmacKey
+        },
+    }
+});
+
+var defaultLevel = 'DEBUG';
+Object.keys(MyScript.Constants.Logger).forEach(function(key) {
+    var name = MyScript.Constants.Logger[key];
+    var logger = MyScript.LoggerConfig.getLogger(name);
+    logger.setLevel(defaultLevel, false);
+});
+window.addEventListener('resize', function() {
+    editorElement.editor.resize();
+});
